4950fa22ddaf8d4396db35e361c6f4e4e71ec796
LuisSempre <luis.silva@viannasempre.com.br>
2023-02-23 23:00:02 -0300

    Init create Account of resgitrations

---
+++ account-registration~/src/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const { v4: uuidv4} = require('uuid')
+const app =  express()
+
+const customers = []
+
+app.use(express.json())
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body
+    const id = uuidv4()
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    })
+
+    return response.status(201).send()
+})
+
+app.listen(7777)
